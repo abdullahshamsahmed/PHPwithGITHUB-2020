5aa9045cc3f9233ce829c2fd54af5853c1664628
TARIF's git area 2020 <tarifahmed5678@gmail.com>
2020-09-10 03:46:00 +0600

    tip: output the keyword value

---
+++ controller/registrationControl.php
@@ -0,0 +1,32 @@
+<?php
+	//dbconnection file include 
+	include('../model/dbcon.php');
+	//condition apply
+	if (isset($_POST['submit'])){
+		$file = $_FILES ['image'];
+		echo "<pre>";
+		print_r($file);
+		echo "</pre>";
+	}
+	$name=$_POST['name'];
+	$add=$_POST['add'];
+	$gender=$_POST['gender'];
+	$city=$_POST['city'];
+	$DOB=$_POST['DOB'];
+	$pass=$_POST['pass'];
+	$email=$_POST['email'];
+	//$ppic=$_POST['ppic'];
+
+	$sql="INSERT INTO registration (name,address,gender,city,DOB,password,email) VALUES ('$name','$add','$gender','$city','$DOB','$pass','$email')";
+	
+	if (!mysqli_query($conn,$sql))
+	{
+		echo"Not inserted";
+	}
+	else
+	{
+		echo "Inserted!!!";
+		//header("refresh:2; url=../registration.php");
+	}
+	
+?>
