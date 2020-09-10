effbc2227f71a6d32bf88876d03d045cdc07524c
TARIF's git area 2020 <tarifahmed5678@gmail.com>
2020-09-10 16:28:53 +0600

    registration controller under post confirmation

--- controller/registrationControl.php
+++ controller/registrationControl.php
@@ -3,6 +3,16 @@
 	include('../model/dbcon.php');
 	//condition apply
 	if (isset($_POST['submit'])){
+		
+		$name=$_POST['name'];
+		$add=$_POST['add'];
+		$gender=$_POST['gender'];
+		$city=$_POST['city'];
+		$DOB=$_POST['DOB'];
+		$pass=$_POST['pass'];
+		$email=$_POST['email'];
+		
+		// <image file controller> 
 		$file = $_FILES ['image'];
 		$fileName = $file['name'];
 		$fileTmp = $file['tmp_name'];
@@ -26,27 +36,19 @@
 			
 			move_uploaded_file($fileTmp,$destinationFile);//tmp file to target file store statements	
 		}
+		// </image file controller> 
 		
-	}
-	$name=$_POST['name'];
-	$add=$_POST['add'];
-	$gender=$_POST['gender'];
-	$city=$_POST['city'];
-	$DOB=$_POST['DOB'];
-	$pass=$_POST['pass'];
-	$email=$_POST['email'];
-	//$ppic=$_POST['ppic'];
-
-	$sql="INSERT INTO registration (name,address,gender,city,DOB,password,email,ppic) VALUES ('$name','$add','$gender','$city','$DOB','$pass','$email','$$destinationFile')";
+		$sql="INSERT INTO registration (name,address,gender,city,DOB,password,email,ppic) VALUES ('$name','$add','$gender','$city','$DOB','$pass','$email','$$destinationFile')";
 	
-	if (!mysqli_query($conn,$sql))
-	{
-		echo"Not inserted";
-	}
-	else
-	{
-		echo "Inserted!!!";
-		header("refresh:2; url=../registration.php");
-	}
-	
+		if (!mysqli_query($conn,$sql))
+		{
+			echo"Not inserted";
+		}
+		else
+		{
+			echo "Inserted!!!";
+			header("refresh:2; url=../registration.php");
+		}
+			
+		}
 ?>
