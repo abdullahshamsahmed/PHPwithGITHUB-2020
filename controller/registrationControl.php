be814ac7c815659c8d7dcf9ff662c19c68b796e4
TARIF's git area 2020 <tarifahmed5678@gmail.com>
2020-09-10 16:22:38 +0600

    image file controller added

--- controller/registrationControl.php
+++ controller/registrationControl.php
@@ -1,7 +1,33 @@
 <?php
 	//dbconnection file include 
 	include('../model/dbcon.php');
-
+	//condition apply
+	if (isset($_POST['submit'])){
+		$file = $_FILES ['image'];
+		$fileName = $file['name'];
+		$fileTmp = $file['tmp_name'];
+		
+		// echo "<pre>";//[starting] make print_r function line by line
+		
+		// print_r($files);// keywords details print out function
+		
+		// echo "</pre>";//[/ending/] make print_r function line by line
+		
+		$fileExt = explode ('.',$fileName);//extention find expolded by "."
+		$fileCheck = strtolower(end($fileExt)); //start to lower : extention name small letter convert
+		
+		$fileExtStore = array ('jpg','png','jpeg');
+		
+		$destinationFile; // public-global variable 
+		
+		if (in_array ($fileCheck,$fileExtStore))
+		{
+			$destinationFile = "../images/".$fileName;
+			
+			move_uploaded_file($fileTmp,$destinationFile);//tmp file to target file store statements	
+		}
+		
+	}
 	$name=$_POST['name'];
 	$add=$_POST['add'];
 	$gender=$_POST['gender'];
@@ -11,7 +37,7 @@
 	$email=$_POST['email'];
 	//$ppic=$_POST['ppic'];
 
-	$sql="INSERT INTO registration (name,address,gender,city,DOB,password,email) VALUES ('$name','$add','$gender','$city','$DOB','$pass','$email')";
+	$sql="INSERT INTO registration (name,address,gender,city,DOB,password,email,ppic) VALUES ('$name','$add','$gender','$city','$DOB','$pass','$email','$$destinationFile')";
 	
 	if (!mysqli_query($conn,$sql))
 	{
