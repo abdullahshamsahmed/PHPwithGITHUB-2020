311509e868b404dc8ba70a8f4261cc131aca8bf1
TARIF's git area 2020 <tarifahmed5678@gmail.com>
2020-09-11 03:00:52 +0600

    login handeler without session is added

---
+++ controller/loginControl.php
@@ -0,0 +1,23 @@
+<?php
+	//dbconnection file include 
+	include('../model/dbcon.php');
+	if (isset ($_POST['submit']))
+	{
+		$name = $_POST['name'];
+		$pass = $_POST['pass'];
+		
+		$sql = "SELECT * FROM registration WHERE name='".$name."' AND password='".$pass."' limit 1";
+		
+		$result = mysqli_query($conn,$sql);
+		
+		if (mysqli_num_rows($result)==1)//mysqli_num_rows() aita check kore j ai result ar konta row ase database a 
+		{
+			header("Location:../curd.php");
+		}
+		else
+		{
+			echo"wrong";
+		}
+		
+	}
+?>
