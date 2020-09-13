76708bd4851be81b0bac4e599a9ef0d20974999c
TARIF's git area 2020 <tarifahmed5678@gmail.com>
2020-09-13 06:11:48 +0600

    edited page is add

---
+++ editInfo.php
@@ -0,0 +1,30 @@
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+	<meta charset="UTF-8">
+	<title></title>
+</head>
+<body>
+	<fieldset>
+		<legend align="center">Editting Info Form</legend>
+		<form action="controller/editInfoController.php" method="POST">
+			<label for=""></label>
+			<input type="" name="" value=""/><br/>
+			<label for=""></label>
+			<input type="" name="" value=""/><br/>
+			<label for=""></label>
+			<input type="" name="" value=""/><br/>
+			<label for=""></label>
+			<input type="" name="" value=""/><br/>
+			<label for=""></label>
+			<input type="" name="" value=""/><br/>
+			<label for=""></label>
+			<input type="" name="" value=""/><br/>
+			<label for=""></label>
+			<input type="" name="" value=""/><br/>
+			<label for=""></label>
+			<input type="" name="" value=""/><br/>
+		</form>
+	</fieldset>
+</body>
+</html>
